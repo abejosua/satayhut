442890244d8fad8257f0db9fd3c8ad5d9a2ce42e
abejosua <abejosua@gmail.com>
2018-03-03 14:06:47 -0600

    added home.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,6 @@ import Hero from '../components/hero.js'
 const IndexPage = ({ data }) => (
   <div>
     <Hero></Hero>
-    {/* <Container>
-      {data.allMarkdownRemark.edges.filter(post => post.node.frontmatter.contentType === 'blog').map(({ node: post }) => (
-        <Card style={{marginBottom: 10}} key={post.id}>
-          <CardBody>
-            <CardTitle><Link to={post.frontmatter.path}>{post.frontmatter.title}</Link></CardTitle>
-            <CardSubtitle style={{marginBottom: 10}}>{post.frontmatter.date}</CardSubtitle>
-            <CardText>{post.excerpt}</CardText>
-          </CardBody>
-        </Card>
-      ))}
-    </Container> */}
   </div>
 
 )
@@ -28,19 +17,5 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          frontmatter {
-            title
-            contentType
-            date(formatString: "MMMM DD, YYYY")
-            path
-          }
-        }
-      }
-    }
   }
 `
